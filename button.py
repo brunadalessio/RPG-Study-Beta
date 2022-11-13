7b7e062e8f236bd339898fe1115301fd439308aa
Bruna D'Aléssio <89991628+ajolq@users.noreply.github.com>
2022-11-13 16:10:54 -0300

    Update button.py

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 import pygame
 
-# Classe dos butões
+# Classe dos botões
 class Button():
 	def __init__(self, surface, x, y, image, size_x, size_y):
 		self.image = pygame.transform.scale(image, (size_x, size_y))
